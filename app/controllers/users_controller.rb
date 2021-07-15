f683b1b6ed61c1309d9797941fa97221e500c951
paseri10mayo <83695208+paseri10mayo@users.noreply.github.com>
2021-07-15 23:47:45 +0900

    ヘッダーの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # def update
-  #   @user = User.find(params[:id])
-  # end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :profile, :profile_image)
+  end
 end

debde9f0eb53c527eb513512d58e3883df923e1d
paseri10mayo <83695208+paseri10mayo@users.noreply.github.com>
2021-07-17 23:46:26 +0900

    createアクション完成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,18 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe)  #レシピの詳細画面に繊維(どのレシピに繊維するのか指定)
+  end
+
   def edit
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:recipe_title, :recipe_details, :image )
   end
 end

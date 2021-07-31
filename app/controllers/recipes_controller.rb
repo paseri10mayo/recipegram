0251fd423671663f016fa0344253bc96ce5c7c2b
paseri10mayo <83695208+paseri10mayo@users.noreply.github.com>
2021-07-31 14:54:51 +0900

    editアクセス制限完成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  
+
   def index
     @recipes = Recipe.all
   end
@@ -22,6 +22,9 @@ class RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
+    if @recipe.user != current_user
+      redirect_to recipes_path, alert: '不正なアクセスです。'
+    end
   end
 
   def update
